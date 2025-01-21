3027bfb12bf10e2a55e9e6932755044ecd93727e
Pedro Junior <nmsystem@gmail.com>
2025-01-21 19:25:57 -0300

    teste API

--- src/app/api/config/apiCredentials.ts
+++ src/app/api/config/apiCredentials.ts
@@ -1,8 +1,6 @@
 const API_CREDENTIALS = {
-    // host: process.env.IXC_HOST  as string,
-    // token: process.env.IXC_TOKEN  as string
-    host: 'https://ixc.malugainfor.net.br',
-    token: '134:4d20134692632db1bae36d494c439b74204e9248bae4ab1e39cdf2a19a2f9cb3'
+    host: process.env.IXC_HOST  as string,
+    token: process.env.IXC_TOKEN  as string
   };
   
   export default API_CREDENTIALS;
